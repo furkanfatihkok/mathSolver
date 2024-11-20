617d8c42f7cd448f3b1cdcde5f0078303d087199
furkanfatihkok <furkankok296@gmail.com>
2024-11-20 14:15:13 +0300

    İlk commit

---
+++ mathSolver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mathSolver
+//
+//  Created by FFK on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
